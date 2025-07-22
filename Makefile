9187f7d84431405623e819a4753d3eba47a377bd
root <root@deletepc.localdomain>
2025-07-22 03:41:52 +0200

    [Dorado-AI-DevOps] Uploaded Changes

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 IMAGE_NAME     := colmap-init
-VERSION        := v0.1.11
+VERSION        := v0.1.12
 REGISTRY       := localhost:5000
 HELM_VALUES    := ../devops-ai-lab/manifests/helm-instant-ngp/values.yaml
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -47,7 +47,6 @@ if ! colmap automatic_reconstructor \
     --workspace_path "$DATA_PATH/colmap" \
     --SiftExtraction.num_threads 12 \
     --SiftMatching.num_threads 12 \
-    --Mapper.num_threads 12 \
     --use_gpu 1; then
     echo "Error: Falló el proceso de generacion de matrices de cámara con COLMAP"
     exit 1
