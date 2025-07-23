e4ee5d60c9a0385e70e3481daef778dec7bdca6c
root <root@deletepc.localdomain>
2025-07-23 17:18:16 +0200

    [Dorado-AI-DevOps] Uploaded Changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,16 +19,9 @@ git clone git@github.com:dorado-ai-devops/ai-nerf-datasets.git /tmp/tmp_cloned
 echo "==> Copiando imágenes al directorio de entrenamiento: $DATA_PATH"
 mkdir -p "$DATA_PATH/images"
 
-if [ "$IMG_COPY_MODE" == "TOTAL" ]; then
-    cp -r /tmp/tmp_cloned/${DATASET_NAME}/images/*.${IMG_TYPE} "$DATA_PATH/images"
-elif [[ "$IMG_COPY_MODE" =~ ^[0-9]+$ ]]; then
-    for i in $(seq 0 $((IMG_COPY_MODE - 1))); do
-        cp "/tmp/tmp_cloned/${DATASET_NAME}/images/r_${i}.${IMG_TYPE}" "$DATA_PATH/images"
-    done
-else
-    echo "Error: IMG_COPY_MODE debe ser 'TOTAL' o un número entero."
-    exit 1
-fi
+# Copiar ds
+cp -r /tmp/tmp_cloned/${DATASET_NAME}/images/*.${IMG_TYPE} "$DATA_PATH/images"
+
 
 echo "==> Ejecutando pipeline COLMAP paso a paso..."
 
